b73edc947d5db44655d7fbea3158cd6640b8aceb
Manikandan Velusamy <mani.app92@gmail.com>
2024-12-03 18:55:19 +0530

    handle with multiple method

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Dashboard data is fetched successfully")
+
+app.get("/user/:id/:name", (req, res) => {
+    console.log(req.params, req.query)
+    res.send({ id: "fdsfdsnfvsdfs", firstName:"Manikandan", lastName: "velusamy" })
 })
-app.get("/test", (req, res) => {
-    res.send("Your test data is loaded")
-})
-app.get("/hello", (req, res) => {
-    res.send("Hello World data is loaded")
-})
+
+
+
 app.listen(8000, () => {
     console.log("Your Server is successfully listening on port 8000")
 });
